863d2247664b19fa397a4bbaf3f170dea225ea0d
Bill Siggelkow <bsiggelkow@mac.com>
2011-08-25 19:06:01 -0400

    Better specs complete

--- spec/jsonify_builder_spec.rb
+++ spec/jsonify_builder_spec.rb
@@ -1,12 +1,73 @@
 require 'spec_helper'
 
-describe 'Rails integration' do
-  it("template class") do 
-    ActionView::Template::Handlers::JsonifyBuilder.should_not be_nil
-  end
+describe 'Jsonify template handler' do
+  
+  Handler = ActionView::Template::Handlers::JsonifyBuilder
+  ViewTemplate = Struct.new(:source)
+  
+  context 'for Rails 3.1' do
+    before do
+      Rails.stub_chain(:application, :config, :jsonify_format).and_return( :plain )
+    end
 
-  describe 'rendering' do
-    it 'should work'
+    let( :handler ) { Handler.new }
+
+    let( :template ) do
+      ViewTemplate.new("json.hello 'world'")
+    end
+    
+    it 'should use JSON as its default output format' do
+      handler.default_format.should == Mime::JSON
+    end
+
+    it 'should compile to JSON' do
+      result = eval( Handler.call template )
+      result.should == '{"hello":"world"}'
+    end
+
+    describe 'when configured with ' do
+      def expect_format(format)
+        Jsonify::Builder
+          .should_receive(:new)
+          .with({:format => format})
+          .and_return( double("json").as_null_object )
+      end
+      
+      describe 'pretty' do
+        before do
+          Rails.stub_chain(:application, :config, :jsonify_format).and_return( :pretty )
+        end
+      
+        it 'should pass pretty to the builder' do
+          expect_format :pretty
+          eval( Handler.call template )
+        end
+      end
+
+      describe 'plain' do
+        before do
+          Rails.stub_chain(:application, :config, :jsonify_format).and_return( :plain )
+        end
+      
+        it 'should pass plain to the builder' do
+          expect_format :plain
+          eval( Handler.call template )
+        end
+      end
+
+      describe 'nothing' do
+        before do
+          Rails.stub_chain(:application, :config)
+        end
+      
+        it 'should pass plain to the builder' do
+          expect_format :plain
+          eval( Handler.call template )
+        end
+      end
+      
+    end
+
   end
 
 end
